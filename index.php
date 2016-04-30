df34a0a1ff516d73b0a9bc7b9f69fd2ff8b29bf1
matthew <properties@users.noreply.github.com>
2016-04-30 21:26:24 +0200

    Homepage

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-
-  $conn = new PDO("mysql:host=localhost;dbname=DBNAME", "DBUSER", "DBPASSWORD");
-
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  $getSites = $conn->prepare("SELECT * FROM `pwd_sites`");
-  $getSites->execute();
-
-  $allSites = $getSites->fetchAll();
+  session_start();
+  $_SESSION['MAINPASSWORD'] = 'PASSWORD1'; // 16char
+  $_SESSION['PHRASE'] = 'PASSWORD2';
 
  ?>
 
@@ -66,6 +60,13 @@
               <a class="im_dialogs_search_clear tg_search_clear ng-hide">
                 <i class="icon icon-search-clear"></i>
               </a>
+
+              <div style="margin-top: 10px;">
+                <button onclick="alertShowSite()" class="btn reply_markup_button" style="float: right;width:48%;">Add site</button>
+                <button onclick="alertShowAccount()" class="btn reply_markup_button" style="float: right;width:48%;margin-right: 11px;">Add account</button>
+              </div>
+
+              <br style="clear:both;">
             </div>
           </div>
 
@@ -74,50 +75,7 @@
               <div class="im_dialogs_scrollable_wrap nano-content" tabindex="-1" style="right: -10px;">
 
                 <!-- Websites -->
-                <ul class="nav nav-pills nav-stacked">
-
-                  <?php
-                  foreach($allSites as $siteInfo) {
-
-                    $getSiteAmount = $conn->prepare("SELECT * from `pwd_accs` WHERE `site` = :url");
-                    $getSiteAmount->bindParam(':url', $siteInfo["url"]);
-                    $getSiteAmount->execute();
-
-                    $siteAmount = $getSiteAmount->rowCount();
-                  ?>
-                    <li class="im_dialog_wrap">
-                      <a class="im_dialog" onclick="getAccounts('<?php echo $siteInfo["url"] ?>')">
-
-                      <div class="im_dialog_meta pull-right text-right">
-                        <div class="im_dialog_date"><?php echo $siteInfo["url"] ?></div>
-                      </div>
-
-                      <div class="im_dialog_photo pull-left" style="border-radius:0%;">
-                        <img class="im_dialog_photo" style="border-radius:0%;" src="<?php echo $siteInfo["img"] ?>">
-                      </div>
-
-                      <div class="im_dialog_message_wrap">
-                        <div class="im_dialog_peer"><span><?php echo $siteInfo["name"] ?></span></div>
-
-                        <div class="im_dialog_message_notyping">
-                          <div class="im_dialog_message">
-                            <span><span>
-                                <span class="im_dialog_chat_from_wrap">
-                                  <span class="im_dialog_chat_from">Accounts</span>
-                                  <span>:</span>
-                                </span>
-                            </span></span>
-
-                            <span>
-                              <span class="im_short_message_text"><?php echo $siteAmount ?></span>
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                <?php } ?>
-              </ul>
+                <ul id="allSites" class="nav nav-pills nav-stacked"></ul>
 
 
           </div>
@@ -127,17 +85,8 @@
 
     <div class="im_history_col_wrap noselect" style="height: 550px;overflow: auto;">
       <div class="im_history_not_selected_wrap">
-        <!-- Accounts -->
-        <script>
-          function getAccounts(url)
-          {
-            $.post( "getAccounts.php" , { url:url } , function( accountInfo ){
-              $('#allAccounts').html(accountInfo.Html);
-            }, "json");
 
-            return false;
-          }
-        </script>
+        <!-- Accounts -->
         <div id="allAccounts" class="im_history_not_selected vertical-aligned" style="text-align: inherit;padding: 10px 10px;">
           <br><br><br><br><center><p>Choose an website</p></center>
         </div>
@@ -152,12 +101,155 @@
     <script>
     new Clipboard('.copy');
 
-    function setColor(btn, color) {
-      var property = document.getElementById(btn);
-      property.style.backgroundColor = "#FFFFFF"
-      }
+    function getSites()
+    {
+      $.post( "system.php" , { ACTION:"getSites" } , function( siteInfo ){
+        $('#allSites').html(siteInfo.Html);
+      }, "json");
 
-</script>
+      return false;
+    }
+
+    function getAccounts(url)
+    {
+      $.post( "system.php" , { ACTION:"getAccounts", url:url } , function( accountInfo ){
+        $('#allAccounts').html(accountInfo.Html);
+      }, "json");
+
+      return false;
+    }
+
+    $(document).ready(function(){
+      getSites();
+    });
+
+    function alertHideAccount() {
+      $("#addAccount").hide();
+    }
+
+    function alertShowAccount() {
+      $("#addAccount").show();
+    }
+
+    function alertHideSite() {
+      $("#addSite").hide();
+    }
+
+    function alertShowSite() {
+      $("#addSite").show();
+    }
+
+    $(document).ready(function(){
+      $("#addSite_form").submit(function() {
+        $.post("system.php", { ACTION:"addSite", name:$("#addSite_name").val(), url:$("#addSite_url").val()}, function() {});
+
+        $("#addSite").hide();
+        getSites();
+
+        return false;
+      });
+
+      $("#addAccount_form").submit(function() {
+        $.post("system.php", { ACTION:"addAccount", url:$("#addAccount_url").val(), email:$("#addAccount_email").val(), username:$("#addAccount_username").val(), password:$("#addAccount_password").val(), extra:$("#addAccount_extra").val() }, function() {});
+
+        $("#addAccount").hide();
+        getSites();
+
+        return false;
+      });
+
+    });
+
+
+  </script>
+
+  <div id="addAccount" style="display:none">
+    <div class="modal-backdrop fade in" style="z-index: 1051;"></div>
+    <div tabindex="-1" role="dialog" class="modal fade md_simple_modal_window mobile_modal in" style="z-index: 1060; display: block;">
+      <div onclick="alertHideAccount()" class="modal_close_wrap"><div class="modal_close"></div></div>
+        <div class="modal-dialog"><div class="modal-content modal-content-animated" style="margin-top: 153.5px;"><div class="md_simple_modal_wrap">
+
+          <div class="md_simple_modal_body">
+
+            <form id="addAccount_form" action="#add" class="modal_simple_form ng-pristine ng-valid">
+
+              <h4 my-i18n="profile_edit_modal_title">Add account</h4>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Site URL</label>
+                <input id="addAccount_url" class="md-input" type="text">
+              </div>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Email</label>
+                <input id="addAccount_email" class="md-input" type="text">
+              </div>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Username</label>
+                <input id="addAccount_username" class="md-input" type="text">
+              </div>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Password</label>
+                <input id="addAccount_password" class="md-input" type="password">
+              </div>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Extra</label>
+                <input id="addAccount_extra" class="md-input" type="text">
+              </div>
+
+              <div class="md_simple_modal_footer">
+                <button type="button" class="btn btn-md" onclick="alertHideAccount()">Cancel</button>
+                <button type="submit" class="btn btn-md btn-md-primary">Save</button>
+              </div>
+
+
+            </form>
+
+          </div>
+
+        </div>
+      </div></div>
+    </div>
+  </div>
+
+  <div id="addSite" style="display:none">
+    <div class="modal-backdrop fade in" style="z-index: 1051;"></div>
+    <div tabindex="-1" role="dialog" class="modal fade md_simple_modal_window mobile_modal in" style="z-index: 1060; display: block;">
+      <div onclick="alertHideSite()" class="modal_close_wrap"><div class="modal_close"></div></div>
+        <div class="modal-dialog"><div class="modal-content modal-content-animated" style="margin-top: 153.5px;"><div class="md_simple_modal_wrap">
+
+          <div class="md_simple_modal_body">
+
+            <form id="addSite_form" action="#add" class="modal_simple_form ng-pristine ng-valid">
+
+              <h4 my-i18n="profile_edit_modal_title">Add Site</h4>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Site URL</label>
+                <input id="addSite_url" class="md-input" type="text">
+              </div>
+
+              <div class="md-input-group md-input-has-value md-input-animated">
+                <label class="md-input-label">Name</label>
+                <input id="addSite_name" class="md-input" type="text">
+              </div>
+
+              <div class="md_simple_modal_footer">
+                <button type="button" class="btn btn-md" onclick="alertHideSite()">Cancel</button>
+                <button type="submit" class="btn btn-md btn-md-primary">Save</button>
+              </div>
+
+            </form>
+
+          </div>
+
+        </div>
+      </div></div>
+    </div>
+  </div>
 
 </body>
 </html>
