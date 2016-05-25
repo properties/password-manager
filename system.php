c27da5746ee3d785ea86316d9d977a3ead64b4ca
matthew <properties@users.noreply.github.com>
2016-05-25 14:56:11 +0200

    Update system.php

--- system.php
+++ system.php
@@ -1,14 +1,13 @@
 <?php
 
-  /*
-     Password Manager
+  #=> Password Manager
+  #=> by Matthew (github: properties)
+  #=> version 1.5.2
 
-     by Matthew (github: properties)
-     version 1.5.2
-  */
-
+  #=> Include config file
   include 'lib/config.php';
 
+  #=> Report no errors
   error_reporting(0);
 
   #=> Session settings
@@ -54,6 +53,7 @@
       return $resultString;
   }
 
+  #=> Block login if failed login
   function blockLogin()
   {
     $getBlock = $databaseConnection->prepare("SELECT * from `pwd_settings` WHERE `name` = 'block' LIMIT 1");
@@ -82,6 +82,7 @@
     return true;
   }
 
+  #=> Add account to database
   if(ACTION == "addAccount")
   {
     $addAccount = $databaseConnection->prepare("INSERT INTO `pwd_accs` (email, username, password, extra, site) VALUES (:email, :username, :password, :extra, :site)");
@@ -92,6 +93,7 @@
     $addAccount->bindParam(':site', encryptAES($_POST["url"]));
     $addAccount->execute();
   }
+  #=> Add site to database
   else if(ACTION == "addSite")
   {
     $addSite = $databaseConnection->prepare("INSERT INTO `pwd_sites` (name, url) VALUES (:name, :url)");
@@ -99,32 +101,25 @@
     $addSite->bindParam(':url', encryptAES($_POST["url"]));
     $addSite->execute();
   }
+  #=> Recieve sites
   else if(ACTION == "getSites")
   {
-
     $getSites = $databaseConnection->prepare("SELECT * from `pwd_sites`");
     $getSites->execute();
     $allSites = $getSites->fetchAll();
 
     foreach($allSites as $siteInfo) {
-
       $getSiteAmount = $databaseConnection->prepare("SELECT * from `pwd_accs` WHERE `site` = :url");
       $getSiteAmount->bindParam(':url', $siteInfo["url"]);
       $getSiteAmount->execute();
       $siteAmount = $getSiteAmount->rowCount();
 
       if(empty($siteInfo["img"])) $siteInfo["img"] = 'http://'. htmlspecialchars(decryptAES($siteInfo["url"])) .'/favicon.ico';
-
-      $htmlCode .= '<li class="im_dialog_wrap"><a class="im_dialog" onclick="getAccounts(\''. htmlspecialchars(decryptAES($siteInfo["url"])) .'\')">
-      <div class="im_dialog_meta pull-right text-right"><div class="im_dialog_date">'. htmlspecialchars(decryptAES($siteInfo["url"])) .'</div></div>
-      <div class="im_dialog_photo pull-left" style="border-radius:0%;"><img class="im_dialog_photo" style="border-radius:0%;" src="'. htmlspecialchars(decryptAES($siteInfo["img"])) .'"></div> <div class="im_dialog_message_wrap">
-      <div class="im_dialog_peer"><span>'. htmlspecialchars(decryptAES($siteInfo["name"])) .'</span></div><div class="im_dialog_message_notyping"><div class="im_dialog_message"><span><span><span class="im_dialog_chat_from_wrap"><span class="im_dialog_chat_from">Accounts</span><span>:</span></span></span></span><span>
-      <span class="im_short_message_text">'. htmlspecialchars($siteAmount) .'</span></span></div></div></div></a></li>';
-
+      $htmlCode .= '<li class="im_dialog_wrap"><a class="im_dialog" onclick="getAccounts(\''. htmlspecialchars(decryptAES($siteInfo["url"])) .'\')"><div class="im_dialog_meta pull-right text-right"><div class="im_dialog_date">'. htmlspecialchars(decryptAES($siteInfo["url"])) .'</div></div><div class="im_dialog_photo pull-left" style="border-radius:0%;"><img class="im_dialog_photo" style="border-radius:0%;" src="'. htmlspecialchars(decryptAES($siteInfo["img"])) .'"></div> <div class="im_dialog_message_wrap"><div class="im_dialog_peer"><span>'. htmlspecialchars(decryptAES($siteInfo["name"])) .'</span></div><div class="im_dialog_message_notyping"><div class="im_dialog_message"><span><span><span class="im_dialog_chat_from_wrap"><span class="im_dialog_chat_from">Accounts</span><span>:</span></span></span></span><span><span class="im_short_message_text">'. htmlspecialchars($siteAmount) .'</span></span></div></div></div></a></li>';
       $returnJson["Html"] = $htmlCode;
     }
-
   }
+  #=> Recieve accounts
   else if(ACTION == "getAccounts")
   {
     $getAccounts = $databaseConnection->prepare("SELECT * from `pwd_accs` WHERE `site` = :url");
@@ -135,24 +130,18 @@
     foreach($allAccounts as $accountInfo)
     {
       for ($i = 1; $i <= strlen(decryptAES($accountInfo["password"])); $i++) $hiddenPassword .= "•";
-      $htmlCode .= '<div class="md_modal_iconed_section_wrap  md_modal_iconed_section_link"style="border-bottom: 1px solid #ebebeb;"><i class="md_modal_section_icon md_modal_section_icon_more"></i><div class="md_modal_section_select_wrap">
-      <div class="dropdown md_modal_section_select"><button data-clipboard-text="' . htmlspecialchars(decryptAES($accountInfo["email"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div>
-      <div class="md_modal_section_param_name"><span style="color: #3a6d99;">Email:</span> ' . htmlspecialchars(decryptAES($accountInfo["email"])) . '</div></div><div class="md_modal_section_select_wrap">
-      <div class="dropdown md_modal_section_select"><button data-clipboard-text="' . htmlspecialchars(decryptAES($accountInfo["username"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div>
-      <div class="md_modal_section_param_name"><span style="color: #3a6d99;">Username:</span> ' . htmlspecialchars(decryptAES($accountInfo["username"])) . '</div></div><div class="md_modal_section_select_wrap">
-      <div class="dropdown md_modal_section_select"><button data-clipboard-text="'. htmlspecialchars(decryptAES($accountInfo["password"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div>
-      <div class="md_modal_section_param_name"><span style="color: #3a6d99;">Password:</span> '. $hiddenPassword . '</div></div><div class="md_modal_section_select_wrap">
-      <div class="dropdown md_modal_section_select"><button data-clipboard-text="' . htmlspecialchars(decryptAES($accountInfo["extra"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div>
-      <div class="md_modal_section_param_name"><span style="color: #3a6d99;">Extra:</span> ' . htmlspecialchars(decryptAES($accountInfo["extra"])) . '</div></div></div>';
+      $htmlCode .= '<div class="md_modal_iconed_section_wrap  md_modal_iconed_section_link"style="border-bottom: 1px solid #ebebeb;"><i class="md_modal_section_icon md_modal_section_icon_more"></i><div class="md_modal_section_select_wrap"><div class="dropdown md_modal_section_select"><button data-clipboard-text="' . htmlspecialchars(decryptAES($accountInfo["email"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div><div class="md_modal_section_param_name"><span style="color: #3a6d99;">Email:</span> ' . htmlspecialchars(decryptAES($accountInfo["email"])) . '</div></div><div class="md_modal_section_select_wrap"><div class="dropdown md_modal_section_select"><button data-clipboard-text="' . htmlspecialchars(decryptAES($accountInfo["username"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div><div class="md_modal_section_param_name"><span style="color: #3a6d99;">Username:</span> ' . htmlspecialchars(decryptAES($accountInfo["username"])) . '</div></div><div class="md_modal_section_select_wrap"><div class="dropdown md_modal_section_select"><button data-clipboard-text="'. htmlspecialchars(decryptAES($accountInfo["password"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div><div class="md_modal_section_param_name"><span style="color: #3a6d99;">Password:</span> '. $hiddenPassword . '</div></div><div class="md_modal_section_select_wrap"><div class="dropdown md_modal_section_select"><button data-clipboard-text="' . htmlspecialchars(decryptAES($accountInfo["extra"])) . '" class="btn btn-link dropdown-toggle copy">Copy</button></div><div class="md_modal_section_param_name"><span style="color: #3a6d99;">Extra:</span> ' . htmlspecialchars(decryptAES($accountInfo["extra"])) . '</div></div></div>';
     }
     $returnJson["Html"] = $htmlCode;
   }
+  #=> Login
   else if(ACTION == "loginAccount")
   {
     $getBlock = $databaseConnection->prepare("SELECT * from `pwd_settings` WHERE `name` = 'block' LIMIT 1");
     $getBlock->execute();
     $currentBlock = $getBlock->fetch();
 
+    #=> Check if block already expired
     if(time() - intval($currentBlock["value2"]) > 600)
     {
       $removeBlock = $databaseConnection->prepare("UPDATE `pwd_settings` SET `value` = '0', `value2` = '' WHERE `name` = 'block'");
@@ -231,6 +220,7 @@
 
     $returnJson["Html"] = $htmlCode;
   }
+  #=> Install site
   else if(ACTION == "installSite")
   {
     $secretAuth = $_POST["secretAuth"];
@@ -249,6 +239,7 @@
     file_put_contents('lib/config.php', $currentConfig . '  define("secret2FA", "'. $secretAuth .'");');
     unlink('install.html');
   }
+  #=> Generate QR code for Google Auth install
   else if(ACTION == "generateQR")
   {
     require_once 'lib/GoogleAuthenticator.php';
@@ -260,6 +251,7 @@
     $returnJson["secretQR"] = $secretAuth;
     $returnJson["imageQR"] = $imageQR;
   }
+  #=> Check if session is logged in
   else if(ACTION == "checkSession")
   {
     if($_SESSION['login'] == 1)
@@ -267,4 +259,5 @@
     $returnJson["Info"] = 'checkSession -> succes';
   }
 
+  #=> Echo result
   echo json_encode($returnJson, JSON_PRETTY_PRINT);
